0198a039445b47e9bd12e976825845d5d72d6458
mers89 <mercymanrique@yahoo.com>
2018-03-21 11:29:16 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/*creating 3 variables & setting up a task*/
+var gulp = require('gulp'); 
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+//
+gulp.task('sass', function(){
+	return gulp.src('app/sass/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest("app/css"))
+	.pipe(browserSync.stream())
+})
+gulp.task('serve', ['sass'], function() {
+	browserSync.init({
+		browser: "google chrome",
+		/*open in chrome*/
+		server: './app'
+	})
+	gulp.watch(['app/sass/*.scss', 'app/sass/partials/*.scss'], ['sass'])
+	gulp.watch('app/css/*.css').on('change', browserSync.reload);
+	gulp.watch('app/*.html').on('change', browserSync.reload)
+})
+gulp.task('default', ['serve']);
